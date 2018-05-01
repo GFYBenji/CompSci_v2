6bbece02d75e5a9f7703cd5ab42b61211efc195e
Benji <benjigarment.appdev@gmail.com>
2018-05-01 08:01:12 +0100

    DB access started

---
+++ src/JuliaScreen.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Benji on 29/04/2018.
+ */
+public class JuliaScreen {
+
+
+}
--- src/MyScreen.java
+++ src/MyScreen.java
@@ -1,12 +1,15 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class MyScreen implements MouseMotionListener {
+public class MyScreen implements MouseMotionListener, ActionListener, KeyListener, MouseListener {
 
     private JFrame window;
     private JLabel main, xCoord, yCoord;
@@ -22,7 +25,6 @@ public class MyScreen implements MouseMotionListener {
         window.setBounds(0,0,800,870);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
-        window.addMouseMotionListener(this);
         window.setLayout(null);
 
         save = new JButton("Save Menu");
@@ -39,6 +41,13 @@ public class MyScreen implements MouseMotionListener {
         main.setBounds(0,0,800,800);
         main.setIcon(new ImageIcon(image));
 
+        save.addActionListener(this);
+        video.addActionListener(this);
+        reset.addActionListener(this);
+        main.addMouseListener(this);
+        main.addKeyListener(this);
+        main.addMouseMotionListener(this);
+
         window.add(main);
         window.add(save);
         window.add(video);
@@ -51,7 +60,7 @@ public class MyScreen implements MouseMotionListener {
     protected BufferedImage makeImage(){
         BufferedImage img = null;
         try{
-            img = ImageIO.read(new File("C:\\Users\\Benji\\IdeaProjects\\CompSci_v2\\Green Square.png"));
+            img = ImageIO.read(new File("Green Square.png"));
             return img;
         }catch(IOException e){
             e.printStackTrace();
@@ -59,9 +68,16 @@ public class MyScreen implements MouseMotionListener {
         }
     }
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
+    protected void saveItem() {
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:ucanaccess://SetStorage.accdb");
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery("INSERT INTO JuliaTable(ID) VALUES(JEFF)");
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
     }
 
     @Override
@@ -69,4 +85,58 @@ public class MyScreen implements MouseMotionListener {
         xCoord.setText("X: " + Integer.toString(e.getX()));
         yCoord.setText("Y: " + Integer.toString(e.getY()));
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == save) {
+            saveItem();
+        }
+    }
+
+    //region Unused Methods
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+    //endregion
 }
